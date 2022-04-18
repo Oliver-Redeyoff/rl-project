418da9532eef74ee1bc2e2eedb343378621d938f
Oliver <oliver.redeyoff@gmail.com>
2022-04-18 16:27:01 +0100

    Fixed issues with dqn

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -1,4 +1,6 @@
+import os
 from collections import deque
+import pickle
 
 import gym
 import random
@@ -7,13 +9,7 @@ import torch
 from torch import nn
 import torch.autograd as autograd
 import torch.optim as optim
-random.seed(0)
-
-
-def get_screen(env):
-    # Returned screen requested by gym is 400x600x3, but is sometimes larger
-    # such as 800x1200x3. Transpose it into torch order (CHW).
-    return env.render(mode='rgb_array').transpose((2, 0, 1))
+from torchvision.transforms import functional
 
 
 class Cnn(nn.Module):
@@ -22,7 +18,7 @@ class Cnn(nn.Module):
         super().__init__()
 
         self.conv_net = nn.Sequential(
-            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4),  # (b x 96 x 55 x 55)
+            nn.Conv2d(in_channels=4, out_channels=96, kernel_size=11, stride=4),  # (b x 96 x 55 x 55)
             nn.LeakyReLU(),
             nn.LocalResponseNorm(size=5, alpha=0.0001, beta=0.75, k=2),  # section 3.3
             nn.MaxPool2d(kernel_size=3, stride=2),  # (b x 96 x 27 x 27)
@@ -41,7 +37,7 @@ class Cnn(nn.Module):
 
         self.classifier = nn.Sequential(
             nn.Dropout(p=0.5, inplace=True),
-            nn.Linear(in_features=(256 * 5 * 3), out_features=4096),
+            nn.Linear(in_features=(512), out_features=4096),
             nn.LeakyReLU(),
             nn.Dropout(p=0.5, inplace=True),
             nn.Linear(in_features=4096, out_features=4096),
@@ -51,7 +47,7 @@ class Cnn(nn.Module):
 
     def forward(self, x):
         x = self.conv_net(x)
-        x = x.view(-1, 256 * 5 * 3)  # reduce the dimensions for linear layer input
+        x = x.view(-1, 512)  # reduce the dimensions for linear layer input
         return self.classifier(x)
 
 class ReplayBuffer:
@@ -61,27 +57,11 @@ class ReplayBuffer:
         self.buffer = deque(maxlen=max_size)
 
     def add_replay(self, state, action, reward, next_state, done):
-        experience = (state, action, np.array([reward]), next_state, done)
+        experience = (state, action, reward, next_state, done)
         self.buffer.append(experience)
 
     def sample(self, batch_size):
-        state_batch = []
-        action_batch = []
-        reward_batch = []
-        next_state_batch = []
-        done_batch = []
-
-        batch = random.sample(self.buffer, batch_size)
-
-        for experience in batch:
-            state, action, reward, next_state, done = experience
-            state_batch.append(state)
-            action_batch.append(action)
-            reward_batch.append(reward)
-            next_state_batch.append(next_state)
-            done_batch.append(done)
-
-        return (state_batch, action_batch, reward_batch, next_state_batch, done_batch)
+        return random.sample(self.buffer, batch_size)
 
     def __len__(self):
         return len(self.buffer)
@@ -93,80 +73,107 @@ class DQNAgent:
         self.net = Cnn(len(actions))
         self.optimizer = optim.Adam(params=self.net.parameters(), lr=0.0001)
         self.replay_buffer = ReplayBuffer(1000)
-        self.gamma = 0.99
+        self.gamma = 0.9
         self.epsilon = 0.5
         self.MSE_loss = nn.MSELoss()
 
-    def get_action(self, state):
-        state = autograd.Variable(torch.from_numpy(state).float().unsqueeze(0))
-        qvals = self.net(state)
-        if (random.random() < self.epsilon):
+    def get_action(self, state, actions):
+        if (len(state) == 4 and random.random() < self.epsilon):
+            state = torch.Tensor(state)
+            qvals = self.net(state)
             action = np.argmax(qvals.detach().numpy())
-            print("Choosed best action {}".format(action))
         else:
-            action = random.randint(0, len(qvals.tolist()[0])-1)
-            print("Choosed random action {}".format(action))
+            action = random.randint(0, len(actions)-1)
 
         return action
 
-    def compute_loss(self, batch):
-        b_states, b_actions, b_rewards, b_next_states, b_dones = batch
-        states = torch.FloatTensor(b_states)
-        actions = torch.LongTensor(b_actions)
-        rewards = torch.FloatTensor(b_rewards)
-        next_states = torch.FloatTensor(b_next_states)
-        dones = torch.FloatTensor(b_dones)
+    def compute_loss(self, experience):
+        state, action, reward, next_state, done = experience
+        state = torch.Tensor(state)
+        next_state = torch.Tensor(next_state)
 
-        curr_Q = self.net.forward(states).gather(1, actions.unsqueeze(1))
-        curr_Q = curr_Q.squeeze(1)
-        next_Q = self.net.forward(next_states)
+        curr_Q = self.net(state)
+        next_Q = self.net(next_state)
         max_next_Q = torch.max(next_Q, 1)[0]
-        expected_Q = rewards.squeeze(1) + (1 - dones) * self.gamma * max_next_Q
+        expected_Q = reward + self.gamma * max_next_Q
 
-        loss = self.MSE_loss(curr_Q.clone(), expected_Q.detach())
+        loss = self.MSE_loss(curr_Q, expected_Q.detach())
         return loss
 
     def train(self, batch_size):
         if (len(self.replay_buffer.buffer) >= batch_size):
             batch = self.replay_buffer.sample(batch_size)
-            loss = self.compute_loss(batch)
+            for experience in batch:
+                loss = self.compute_loss(experience)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
 
-            self.optimizer.zero_grad()
-            loss.backward()
-            self.optimizer.step()
+class Environment:
 
+    def __init__(self):
+        self.gym_env = gym.make('ALE/Pong-v5', frameskip=4) #, render_mode="human"
+        self.gym_env.seed(0)
+        self.actions = [i for i in range(0, self.gym_env.action_space.n)]
+        self.state = []
+
+    def reset(self):
+        frame = self.gym_env.reset()
+        self.add_frame_to_state(frame)
+
+    def take_action(self, action):
+        frame, reward, done, _ = self.gym_env.step(action)
+        self.add_frame_to_state(frame)
+        return (reward, done)
+
+    def add_frame_to_state(self, frame):
+        transposed = frame.transpose(2, 0, 1)
+        as_tensor = torch.Tensor(transposed)
+        grey = functional.rgb_to_grayscale(as_tensor)
+        downsampled = functional.resize(grey, [110, 84])
+        self.state.append(downsampled)
+        if (len(self.state) > 4):
+            self.state.pop(0)
+
+    def get_state(self):
+        return torch.stack(self.state).squeeze(1)
+    
 
 def main():
-    env = gym.make('Pong-v0') #, render_mode="human"
-    env.seed(0)
-    actions = [i for i in range(0, env.action_space.n)]
-    agent = DQNAgent(actions)
+
+    if os.path.exists("rewards.txt"):
+        os.remove("rewards.txt")
+
+    env = Environment()
+    agent = DQNAgent(env.actions)
 
     episode_count = 100
-    rewards = []
     for _ in range(episode_count):
         episode_reward = 0
         env.reset()
-        current_state = get_screen(env)
 
         while (True):
-            action = agent.get_action(current_state)
-            old_state = current_state
-            _, reward, done, _ = env.step(action)
-            current_state = get_screen(env)
+            action = agent.get_action(env.get_state(), env.actions)
+            old_state = env.get_state()
+            reward, done = env.take_action(action)
+            
             episode_reward += reward
-
-            agent.replay_buffer.add_replay(old_state, action, reward, current_state, done)
-            agent.train(4)
-
-            print('Took action {} and got reward {}'.format(action, reward))
+            if (len(old_state) == 4):
+                agent.replay_buffer.add_replay(old_state, action, reward, env.get_state(), done)
+                agent.train(1)
 
             if done:
-                print('Episode done, rewards from episode: %s' % episode_reward)
-                rewards.append(episode_reward)
+                with open('rewards.txt', 'a') as f:
+                    f.write(str(episode_reward))
+                    f.write('\n')
+
+                with open('agent.pkl', 'wb') as outp:
+                    pickle.dump(agent, outp, pickle.HIGHEST_PROTOCOL)
+
+                episode_reward = 0
+                agent.epsilon *= 0.99
                 break
     
-
 
 if __name__ == '__main__':
     main()
