932762cd0881520fdb0cb896d748c8895800c549
Will Cruse <willcruse123@protonmail.com>
2022-04-23 14:23:16 +0100

    dqn

--- dqn/dqn.py
+++ dqn/dqn.py
@@ -12,47 +12,16 @@ import torch.autograd as autograd
 import torch.optim as optim
 from torchvision.transforms import functional
 
-
-class MemoryNetwork(nn.Module):
-
-    def __init__(self, classes_count):
-        super().__init__()
-
-        self.classifier = nn.Sequential(
-            nn.Dropout(p=0.5),
-            nn.Linear(in_features=128, out_features=512),
-            nn.ReLU(),
-            nn.Dropout(p=0.5),
-            nn.Linear(in_features=512, out_features=4096),
-            nn.ReLU(),
-            nn.Linear(in_features=4096, out_features=4096),
-            nn.ReLU(),
-            nn.Linear(in_features=4096, out_features=classes_count),
-        )
-
-    def forward(self, x):
-        # x = self.conv_net(x)
-        # x = x.view(-1, 512)  # reduce the dimensions for linear layer input
-        return self.classifier(x)
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class ImageNetwork(nn.Module):
     def __init__(self, classes_count) -> None:
         super().__init__()
 
         self._cnn = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(64, 192, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            nn.Conv2d(192, 384, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(384, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(in_channels=4, out_channels=4, kernel_size=4, stride=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+           # nn.Conv2d(in_channels=4, out_channels = 8, kernel_size=5, stride=2)
         )
 
         self._action_network = nn.Sequential(
@@ -73,11 +42,11 @@ class ImageNetwork(nn.Module):
         # print(type(image))
         transposed = image.transpose(1, 3)
         # print(transposed.shape)
-        # grey = functional.rgb_to_grayscale(transposed)
+        grey = functional.rgb_to_grayscale(transposed)
+        thresh = torch.threshold(grey, 83, 0)
         # downsampled = functional.resize(grey, [110, 84])
 
-        return transposed
-        return image
+        return thresh
 
     def forward(self, input_image):
         convoluted_image = self._cnn(input_image)
@@ -90,8 +59,8 @@ class ImageNetwork(nn.Module):
 class ReplayBuffer:
 
     def __init__(self, max_size):
-        # self.max_size = max_size
-        self.buffer = deque()
+        self.max_size = max_size
+        self.buffer = deque(maxlen=max_size)
 
     def add_replay(self, state, action, reward, next_state, done):
         experience = (state, action, reward, next_state, done)
@@ -104,68 +73,12 @@ class ReplayBuffer:
         return len(self.buffer)
 
 class DQNAgent:
-    def __init__(self, actions, epsilon=0.5, gamma=0.9):
-        self.classes = actions
-        self.net = MemoryNetwork(len(self.classes))
-        self.optimizer = optim.Adam(params=self.net.parameters(), lr=0.0001)
-        self.replay_buffer = ReplayBuffer(1000)
-        self.gamma = gamma
-        self.epsilon = epsilon
-        self.MSE_loss = nn.MSELoss()
-        self.huber_loss = nn.HuberLoss()
-
-    def get_action(self, state, actions):
-        should_explore = random.random() < self.epsilon
-
-        if should_explore:
-            return random.choice(actions)
-
-        result = self.net(state)
-        reduced = result.sum(dim=0)
-        selected_action = torch.argmax(reduced)
-
-        return selected_action
-
-    def compute_loss(self, experience):
-        state, action, reward, next_state, done = experience
-        reward = torch.Tensor([reward])
-
-        if done:
-            y_j = reward
-        
-        else:
-            state = torch.Tensor(state)
-            next_state = torch.Tensor(next_state)
-            result = self.net(next_state)
-            max_next_reward = torch.max(result.sum(dim=0))
-
-            y_j = reward + self.gamma * max_next_reward
-
-        loss = self.huber_loss(y_j, reward)
-        return loss
-
-    def train(self, batch_size):
-        if len(self.replay_buffer) < batch_size:
-            return
-
-        batch = self.replay_buffer.sample(batch_size)
-        self.optimizer.zero_grad()
-
-        for experience in batch:
-            loss = self.compute_loss(experience)
-            if loss.requires_grad:
-                # loss.retain_grad()
-                loss.backward()
-
-        self.optimizer.step()
-
-class ImageDQNAgent(DQNAgent):
 
     def __init__(self, actions, epsilon=0.5, gamma=0.9):
         self.classes = actions
         self.net = ImageNetwork(len(actions))
         self.optimizer = optim.Adam(params=self.net.parameters(), lr=0.0001)
-        self.replay_buffer = ReplayBuffer(1000)
+        self.replay_buffer = ReplayBuffer(10000)
         self.gamma = gamma
         self.epsilon = epsilon
         self.MSE_loss = nn.MSELoss()
@@ -178,6 +91,7 @@ class ImageDQNAgent(DQNAgent):
             return random.choice(actions)
 
         result = self.net(state)
+        print(result)
         reduced = result.sum(dim=0)
         selected_action = torch.argmax(reduced)
 
@@ -187,8 +101,10 @@ class ImageDQNAgent(DQNAgent):
         state, action, reward, next_state, done = experience
         state = torch.Tensor(state)
         next_state = torch.Tensor(next_state)
-
         reward = torch.Tensor([reward])
+
+        expected_result = self.net(state)
+        q_value = expected_result[action]
 
         if done:
             y_j = reward
@@ -199,7 +115,7 @@ class ImageDQNAgent(DQNAgent):
 
             y_j = reward + self.gamma * max_next_reward
 
-        loss = self.huber_loss(y_j, reward)
+        loss = self.huber_loss(q_value, y_j)
         return loss
 
     def train(self, batch_size):
@@ -207,17 +123,17 @@ class ImageDQNAgent(DQNAgent):
             return
 
         batch = self.replay_buffer.sample(batch_size)
+        self.optimizer.zero_grad()
         for experience in batch:
             loss = self.compute_loss(experience)
             if loss.requires_grad:
-                self.optimizer.zero_grad()
-                loss.retain_grad()
                 loss.backward()
-                self.optimizer.step()
+
+        self.optimizer.step()
 
 class Environment:
 
-    def __init__(self, frame_skip, environment_name='ALE/Pong-ram-v5'):
+    def __init__(self, frame_skip, environment_name='ALE/Pong-v5'):
         self.frame_skip = frame_skip
         self.gym_env = gym.make(environment_name) #, render_mode="human"
         self.gym_env.seed(0)
@@ -229,10 +145,12 @@ class Environment:
         self.state = []
 
         frame = self.gym_env.reset()
+        frame = ImageNetwork.preprocess_image(frame)
         self.add_frame_to_state(frame)
 
     def take_action(self, action):
         frame, reward, done, _ = self.gym_env.step(action)
+        frame = ImageNetwork.preprocess_image(frame)
         self.add_frame_to_state(frame)
         return (reward, done)
 
@@ -247,24 +165,16 @@ class Environment:
     def get_state(self):
         return torch.stack(self.state).squeeze(1)
 
-    
-    def _pre_process(self, frame):
-        transposed = frame.transpose(2, 0, 1)
-        as_tensor = torch.Tensor(transposed)
-        grey = functional.rgb_to_grayscale(as_tensor)
-        downsampled = functional.resize(grey, [110, 84])
-
-        return downsampled
-
-def image_main(frame_skip=4, batch_size=4, num_episodes=1000):
+def main(frame_skip=4, batch_size=4, num_episodes=1000):
     if os.path.exists("rewards_image.txt"):
         os.remove("rewards_image.txt")
 
     env = Environment(frame_skip, environment_name='ALE/Pong-v5')
-    agent = ImageDQNAgent(env.actions)
+    agent = DQNAgent(env.actions)
 
-    for _ in range(int(num_episodes)):
+    for episode_num in range(int(num_episodes)):
         episode_reward = 0
+        start_time = time.perf_counter()
         env.reset()
 
         done = False
@@ -284,59 +194,22 @@ def image_main(frame_skip=4, batch_size=4, num_episodes=1000):
                 agent.train(batch_size)
 
             if done:
-                print(episode_reward)
-                with open('rewards_image.txt', 'a') as f:
-                    f.write(str(episode_reward))
-                    f.write('\n')
-
-                with open('agent_image.pkl', 'wb') as outp:
-                    pickle.dump(agent, outp, pickle.HIGHEST_PROTOCOL)
-
-                episode_reward = 0
-                agent.epsilon *= 0.99
-
-
-def main(frame_skip=4, batch_size=4, num_episodes=1000):
-
-    if os.path.exists("rewards.txt"):
-        os.remove("rewards.txt")
-
-    env = Environment(frame_skip)
-    agent = DQNAgent(env.actions, epsilon=0.75, gamma=0.2)
-
-    for episode_num in range(int(num_episodes)):
-        start_time = time.perf_counter()
-        episode_reward = 0
-        env.reset()
-
-        done = False
-        while not done:
-            current_state = env.get_state()
-            action = agent.get_action(current_state, env.actions)
-            reward, done = env.take_action(action)
-            
-            episode_reward += reward
-
-            if (len(current_state) == frame_skip):
-                agent.replay_buffer.add_replay(current_state, action, reward, env.get_state(), done)
-                agent.train(batch_size)
-
-            if done:
                 end_time = time.perf_counter()
-                print(f"episode: {episode_num+1} reward {episode_reward} time taken: {end_time-start_time}")
+                print(f"episode: {episode_num+1} reward: {episode_reward} time taken: {end_time-start_time} epsilon: {agent.epsilon}")
                 with open('rewards.txt', 'a') as f:
                     f.write(str(episode_reward))
                     f.write('\n')
 
-                if episode_num % 50 == 0:
-                    with open('agent.pkl', 'wb') as outp:
-                        pickle.dump(agent, outp, pickle.HIGHEST_PROTOCOL)
+        if episode_num % 50 == 0:
+            with open('agent.pkl', 'wb') as outp:
+                pickle.dump(agent, outp, pickle.HIGHEST_PROTOCOL)
 
-                if episode_num % 10:
-                    agent.epsilon *= 0.9
+        if episode_num % 10 == 10:
+            agent.epsilon *= 0.9
+
 
 if __name__ == '__main__':
-    main(num_episodes=2e7)
+    main(num_episodes=10000)
 
 
 # class DQNAgent:
